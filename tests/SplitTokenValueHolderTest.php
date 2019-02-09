6a1665b8bf7e5f726c5a5b1b95b63ff6f626572b
Sebastiaan Stok <s.stok@rollerscapes.net>
2019-02-09 14:36:56 +0100

    style: Simplified copyright header

--- tests/SplitTokenValueHolderTest.php
+++ tests/SplitTokenValueHolderTest.php
@@ -3,10 +3,6 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the Rollerworks SplitToken Component.
- *
- * (c) Sebastiaan Stok <s.stok@rollerscapes.net>
- *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
