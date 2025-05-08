48bde81f6796ac28bce35d9e5858c097209aeebd
Sebastiaan Stok <s.stok@rollerscapes.net>
2025-05-08 21:46:45 +0200

    Initial commit for config

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+// See https://hupkit.github.io/hupkit/config.html
+
+return [
+    'branches' => [],
+    'host' => 'github.com',
+    'repository' => 'rollerworks/split-token',
+    'schema_version' => 2,
+    'main_branch' => 'main',
+];
